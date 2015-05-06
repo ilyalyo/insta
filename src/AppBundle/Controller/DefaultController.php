91e4cad907916791d238b59795e75837ff45dcb3
ilya <ilya4041@yandex.ru>
2015-05-06 14:17:03 +0300

    fix

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -126,8 +126,8 @@ class DefaultController extends Controller
 
             $proxy = $em->getRepository('AppBundle:Proxy')->findAll();
             $proxy_count=$account->getId() % count($proxy);
-            var_dump(count($proxy));
-            var_dump($account->getId());
+            var_dump($proxy);
+            var_dump($proxy[$proxy_count]->getId());
             $account->setProxy($proxy[$proxy_count]->getId());
 
             $em->persist($account);

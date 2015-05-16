6fb95f01d335e141b4433e8215f31ec08282c484
ilya <ilya4041@yandex.ru>
2015-05-16 17:43:10 +0300

    add unfollow

---
+++ src/AppBundle/Command/Casper/check_auth.js
@@ -0,0 +1,23 @@
+//var casper = require('casper').create();
+
+var uname=casper.cli.get(0).toString();
+var pass=casper.cli.get(1).toString();
+
+var auth_url='https://instagram.com/accounts/login';
+
+casper.start().thenOpen(auth_url, function() {
+    this.wait(2000, function() {
+        this.fillSelectors('form', {
+               'input[name="username"]':  uname,
+               'input[name="password"]':  pass
+        }, true);
+     });
+
+     this.wait(1000, function() { casper.echo(1);});
+});
+
+casper.then(function() {
+    this.test.assert(this.exists('.current-user-avatar'));
+});
+
+casper.run();

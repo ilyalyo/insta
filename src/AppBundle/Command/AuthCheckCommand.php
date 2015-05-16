67da7ce66f1e0c045b7201c779e1aab74cf5ba52
ilya <ilya4041@yandex.ru>
2015-05-16 18:00:27 +0300

    add unfollow

--- src/AppBundle/Command/AuthCheckCommand.php
+++ src/AppBundle/Command/AuthCheckCommand.php
@@ -6,7 +6,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class AuthCommand extends ContainerAwareCommand
+class AuthCheckCommand extends ContainerAwareCommand
 {
     protected function configure()
     {

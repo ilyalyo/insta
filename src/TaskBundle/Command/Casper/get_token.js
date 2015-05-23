9bb7fb83ec9a0c5076197717850bd369201684b7
ilya <ilya4041@yandex.ru>
2015-05-23 07:00:47 +0300

    get token

---
+++ src/TaskBundle/Command/Casper/get_token.js
@@ -0,0 +1,46 @@
+var casper = require('casper').create();
+
+var uname=casper.cli.get(0).toString();
+var pass=casper.cli.get(1).toString();
+var client_id=casper.cli.get(2).toString();
+
+
+var auth_url='https://instagram.com/accounts/login/';
+var manage='https://instagram.com/accounts/manage_access/';
+
+casper.start().thenOpen(auth_url,
+    function() {
+        this.wait(2000, function() {
+            this.fillSelectors('form', {
+                'input[name="username"]': uname,
+                'input[name="password"]':  pass
+            }, true);
+        });
+
+        this.wait(1000, function() {});
+    });
+
+//1 easytogo
+//2 extragram
+//3 ficonosquare
+
+casper.thenOpen(manage, function() {
+    if(client_id == 1){
+        this.evaluate(function(){
+            this.echo( $('#client_easytogo').find('form').find('input').val() );
+        })
+    }
+    else if (client_id == 2) {
+        this.evaluate(function(){
+            this.echo( $('#client_extragram').find('form').find('input').val() );
+        })
+    }
+    else {
+        this.evaluate(function(){
+            this.echo( $('#client_ficonosquare').find('form').find('input').val() );
+        })
+    }
+
+});
+
+casper.run();

c3e0822e33e7caef81bdba683e7b3e30e99eb745
ilya <ilya4041@yandex.ru>
2015-06-01 00:23:36 +0300

    fix bug

--- src/AppBundle/Entity/Token.php
+++ src/AppBundle/Entity/Token.php
@@ -36,7 +36,7 @@ class Token
     protected $url;
 
     /**
-     * @ORM\Column(type="string", length=100,unique=true)
+     * @ORM\Column(type="string", length=100)
      */
     protected $client;
 

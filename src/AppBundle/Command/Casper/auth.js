ad0721c8dbba70dae297771087a4a3145ef71b84
ilya <ilya4041@yandex.ru>
2015-06-03 21:11:17 +0300

    try new provider Crowdfire

--- src/AppBundle/Command/Casper/auth.js
+++ src/AppBundle/Command/Casper/auth.js
@@ -6,7 +6,7 @@ var id=casper.cli.get(2).toString();
 
 var auth_url='https://instagram.com/oauth/authorize/?client_id=6e336200a7f446a78b125602b90989cc&response_type=code&scope=likes+comments+relationships&redirect_uri=http://instastellar.su/get_token?account_id=' + id;
 var auth_url2='https://instagram.com/oauth/authorize/?client_id=c02d1c473e53485d946a1c44d3daf8d2&redirect_uri=http://extragr.am/sessions/callback&response_type=code&scope=comments+likes+relationships';
-var auth_url3='https://instagram.com/oauth/authorize?client_id=d9494686198d4dfeb954979a3e270e5e&redirect_uri=http%3A%2F%2Ficonosquare.com&response_type=code&scope=likes+comments+relationships';
+var auth_url3='https://accounts.crowdfireapp.com/auth/instagram';
 var auth_url4='https://instagram.com/oauth/authorize?client_id=67be0e2777b94bc9bdacf4e2680d77d4&response_type=code&redirect_uri=http%3A%2F%2Flogingram.com%2Findex.php&scope=likes+comments+relationships';
 
 casper.start().thenOpen(auth_url, function() {

411b231841027cb879f9d70ad1271a89fcdc25a6
ilya <ilya4041@yandex.ru>
2015-06-04 19:29:31 +0300

    pursh

--- src/AppBundle/Controller/PurchaseController.php
+++ src/AppBundle/Controller/PurchaseController.php
@@ -27,23 +27,34 @@ class PurchaseController extends Controller
      */
     public function purchaseSuccessAction(Request $request)
     {
-        $login = $request->get('WMI_MERCHANT_ID');
-        $user_id = $request->get('WMI_CUSTOMER_ID');
-        $login = $request->get('WMI_PAYMENT_AMOUNT');
-        $login = $request->get('WMI_COMMISSION_AMOUNT');
-        $login = $request->get('WMI_CURRENCY_ID');
-        $login = $request->get('WMI_PAYMENT_NO');
-        $login = $request->get('WMI_ORDER_STATE');
-        $login = $request->get('WMI_SIGNATURE');
-
+        $params=[];
+        $params['notification_type'] = $request->get('notification_type');
+        $params['operation_id']  = $request->get('operation_id');
+        $params['amount']  = $request->get('amount');
+        $params['withdraw_amount'] = $request->get('withdraw_amount');
+        $params['currency'] = $request->get('currency');
+        $params['datetime'] = $request->get('datetime');
+        $params['sender']= $request->get('sender');
+        $params['codepro']= $request->get('codepro');
+        $params['label'] = $request->get('label');
+        $params['notification_secret'] = 'nzyqKS9YdRwGoNZ+OrFfQh0D';
+        $sha1 = $request->get('sha1_hash');
 
         $em = $this->getDoctrine()->getManager();
-        $user = $em->getRepository('UserBundle:User')->find($user_id);
-        $date = new \DateTime();
-        $date->add(new \DateInterval('P30D'));
-        $user->setValidUntil($date);
-
-        return new JsonResponse('WMI_RESULT=OK');
+        $user = $em->getRepository('UserBundle:User')->find($params['label']);
+        if(isset($user)){
+            $str="";
+            foreach($params as $k => $v){
+                $str += $v;
+            }
+            if(sha1($str) == $sha1){
+                $date = new \DateTime();
+                $date->add(new \DateInterval('P30D'));
+                $user->setValidUntil($date);
+                return new JsonResponse('200 OK');
+            }
+        }
+        return new JsonResponse('400');
     }
 
 

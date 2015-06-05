678ade1ef840defd58b859005bc09895f29bb7f1
ilya <ilya4041@yandex.ru>
2015-06-05 17:13:44 +0300

    resets pass

---
+++ src/UserBundle/EventListener/PasswordResettingListener.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace UserBundle\EventListener;
+
+use FOS\UserBundle\FOSUserEvents;
+use FOS\UserBundle\Event\FormEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+/**
+ * Listener responsible to change the redirection at the end of the password resetting
+ */
+class PasswordResettingListener implements EventSubscriberInterface {
+    private $router;
+
+    public function __construct(UrlGeneratorInterface $router) {
+        $this->router = $router;
+    }
+
+    public static function getSubscribedEvents() {
+        return [
+            FOSUserEvents::RESETTING_RESET_SUCCESS => 'onPasswordResettingSuccess',
+        ];
+    }
+
+    public function onPasswordResettingSuccess(FormEvent $event) {
+        $url = $this->router->generate('accounts');
+        $event->setResponse(new RedirectResponse($url));
+    }
+}

97bdbeed9f5b9ee0e1e4ad8d6ae3d4764197d982
ilya <ilya4041@yandex.ru>
2015-06-11 17:49:27 +0300

    add flash msg

---
+++ src/UserBundle/EventListener/LoginListener.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace UserBundle\EventListener;
+
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
+use Symfony\Component\Security\Core\SecurityContext;
+use Doctrine\Bundle\DoctrineBundle\Registry as Doctrine; // for Symfony 2.1.0+
+// use Symfony\Bundle\DoctrineBundle\Registry as Doctrine; // for Symfony 2.0.x
+
+/**
+ * Custom login listener.
+ */
+class LoginListener
+{
+    /** @var \Symfony\Component\Security\Core\SecurityContext */
+    private $securityContext;
+
+    /** @var \Doctrine\ORM\EntityManager */
+    private $em;
+
+    private $session;
+
+    /**
+     * Constructor
+     *
+     * @param SecurityContext $securityContext
+     * @param Doctrine        $doctrine
+     */
+    public function __construct(SecurityContext $securityContext, Doctrine $doctrine, Session $session)
+    {
+        $this->securityContext = $securityContext;
+        $this->em              = $doctrine->getEntityManager();
+        $this->session              = $session;
+    }
+
+    /**
+     * Do the magic.
+     *
+     * @param InteractiveLoginEvent $event
+     */
+    public function onSecurityInteractiveLogin(InteractiveLoginEvent $event)
+    {
+        if ($this->securityContext->isGranted('IS_AUTHENTICATED_FULLY')) {
+            // user has just logged in
+        }
+
+        if ($this->securityContext->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
+            // user has logged in using remember_me cookie
+        }
+
+        // do some other magic here
+        $user = $event->getAuthenticationToken()->getUser();
+
+        if($user->isExpired())
+            $this->session->getFlashBag()->add(
+                'notice',
+                "Бесплатный период использования сервиса закончился, для продления аккаунта перейдите на страницу покупки, либо свяжитесь с нашей технической поддержкой в группе вконтакте, если Вы еще не успели вполне опробовать наш ресурс и испытать весь его функционал!"
+            );
+    }
+}

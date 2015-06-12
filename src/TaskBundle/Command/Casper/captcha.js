394440ec400b8a61b4203a7d27eba373a9eb4050
ilya <ilya4041@yandex.ru>
2015-06-12 17:42:29 +0300

    add captcha

---
+++ src/TaskBundle/Command/Casper/captcha.js
@@ -0,0 +1,34 @@
+var casper = require('casper').create();
+
+var uname=casper.cli.get(0).toString();
+var pass=casper.cli.get(1).toString();
+var id=casper.cli.get(2).toString();
+
+var url='https://instagram.com/accounts/login';
+
+casper.start().thenOpen(url, function() {
+    this.wait(2000, function() {
+        this.fillSelectors('form', {
+            'input[name="username"]':  uname,
+            'input[name="password"]':  pass
+        }, true);
+    });
+
+    this.wait(1000, function() { casper.echo('');});
+});
+
+casper.then(function() {
+    if(this.exists('#recaptcha_challenge_image')){
+        this.captureSelector('/var/www/instastellar/tasks/' + id + 'captcha.png', '#recaptcha_challenge_image');
+    }
+    this.wait(60000, function() { casper.echo('');});
+});
+
+/*
+casper.then(function() {
+    try{
+        this.click('.button-green');
+    }catch(e){}
+});
+*/
+casper.run();

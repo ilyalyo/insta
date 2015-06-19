41af55c0336f051607e7a247435c85a06c589ed0
ilya <ilya4041@yandex.ru>
2015-06-19 19:53:17 +0300

    add analytic

---
+++ src/AdminBundle/Controller/AnalyticController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace AdminBundle\Controller;
+
+use AppBundle\Entity\Support;
+use AppBundle\Form\Type\SupportType;
+use Doctrine\ORM\EntityRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class AnalyticController extends Controller
+{
+    /**
+     * @Security("has_role('ROLE_ADMIN')")
+     * @Route("/admin/analytic", name="admin_analytic")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $users = $em->getRepository('UserBundle:User')->findAll();
+        $index = 0;
+        foreach ($users as $u) {
+            $userDates[] = $u->getCreatedAt();
+            $usersCount[] = '[' . $u->getCreatedAt()->getTimestamp() . ',' . $index++ . '],';
+        }
+
+        return $this->render(
+            'admin/analytic.html.twig',
+            [
+                'userDates' => $userDates,
+                'usersCount' => $usersCount,
+            ]
+        );
+    }
+}

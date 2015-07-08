868744a230fa2b3a1bd31b9170b12b032850fb23
ilya <ilya4041@yandex.ru>
2015-07-08 03:35:57 +0300

    fix db name

--- cron/manager.php
+++ cron/manager.php
@@ -68,7 +68,7 @@ function connect(){
     if (!$connection){
         die("Database Connection Failed" . mysql_error());
     }
-    $select_db = mysql_select_db('instastellar-dev');
+    $select_db = mysql_select_db('symfony');
     if (!$select_db){
         die("Database Selection Failed" . mysql_error());
     }

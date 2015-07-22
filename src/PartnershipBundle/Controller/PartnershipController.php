dbef4d03807e32d2647dfefcba8fe41a2bfd97e6
gitromand <romick3@mail.ru>
2015-07-22 22:28:24 +0300

    Major update - PartnershipBundle
    Добавлен новый бандл, отвечающий за работу с партнеркой.

---
+++ src/PartnershipBundle/Controller/PartnershipController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace PartnershipBundle\Controller;
+
+/*¬от тут нужные таблицы не забыть указать:*/
+use AppBundle\Entity\Support;
+use AppBundle\Form\Type\SupportType;
+use Doctrine\ORM\EntityRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class PartnershipController extends Controller
+{
+    /**
+     * @Security("has_role('ROLE_PARTNER')")
+     * @Route("/partnership", name="partnership")
+     */
+    public function indexAction()
+    {
+        return $this->render(
+            'partnership/index.html.twig'
+        );
+    }
+}

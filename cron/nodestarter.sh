d7709ccc24bf625fd8e1dc7026160476ffbb43dc
gitromand <romick3@mail.ru>
2015-07-25 04:50:55 +0300

    fix nodestarter

--- cron/nodestarter.sh
+++ cron/nodestarter.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 PIDS=()
-for pid in `ps aux | egrep "[n]ode" | awk '{print $2}'`;
+for pid in `ps aux | egrep "[s]erver.js" | awk '{print $2}'`;
 do
     PIDS+=($pid);
 done
 if [ ${#PIDS[@]} == 0 ];
 then
-    cd /var/www/instastellar/
-    nohup node server.js &
+    cd /var/www/instastellar
+    node server.js &
     #adding 3 hours, cuz we're in the MSK timezone
     curdate=`date +"%d.%m.%Y %T" -d "+ 3 hours"`
-    echo $curdate>>/var/www/instastellar/nodestart.log
-    echo "node restarted at $curdate"
+    echo $curdate>>/var/log/nodestarter.log
 fi

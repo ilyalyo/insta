f07a27b3f6bf34c3dd3ff1f3705b4cc574fd1a60
ilya <ilya4041@yandex.ru>
2015-09-01 23:42:03 +0300

    fix

---
+++ src/TaskBundle/Command/WriteCommand.php
@@ -0,0 +1,32 @@
+<?php
+namespace TaskBundle\Command;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class WriteCommand extends ContainerAwareCommand
+{
+
+
+    protected function configure()
+    {
+        $this
+            ->setName('demo:write')
+            ->setDescription('Greet someone')
+            ->addArgument(
+                'id',
+                InputArgument::OPTIONAL,
+                'Who do you want to greet?'
+            )
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $id = $input->getArgument('id');
+        $file = __DIR__ . "/follow.php";
+        shell_exec("php $file '" . $id . "' > /dev/null &");
+    }
+}

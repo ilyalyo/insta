0338e2bf7cfa6fee9018dffab0d29ef98b2ca520
ilya <ilya4041@yandex.ru>
2015-09-03 03:30:00 +0300

    add check

---
+++ src/AdminBundle/Command/CheckCommand.php
@@ -0,0 +1,22 @@
+<?php
+namespace AdminBundle\Command;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class CheckCommand extends ContainerAwareCommand
+{
+
+    protected function configure()
+    {
+        $this->setName('task:get_list');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $text = shell_exec("ps -aux | grep follow.php |  awk '{print $13}' | grep -v follow | sort");
+        $output->writeln($text);
+    }
+}

2454879f6d64181b4e713efc4405fd060c1503ac
ilya <ilya4041@yandex.ru>
2016-04-11 01:00:27 +0300

    te

--- dump.sql
+++ dump.sql
@@ -1,13 +1,15 @@
-
 --
--- Dumping data for table `wptests_links`
+-- Table structure for table `pmt_attempts`
 --
 
-INSERT INTO `wptests_links` (`link_id`, `link_url`, `link_name`, `link_image`, `link_target`, `link_description`, `link_visible`, `link_owner`, `link_rating`, `link_updated`, `link_rel`, `link_notes`, `link_rss`) VALUES
-(1, 'http://codex.wordpress.org/', 'Documentation', '', '', '', 'Y', 1, 0, '0000-00-00 00:00:00', '', '', ''),
-(2, 'http://wordpress.org/news/', 'WordPress Blog', '', '', '', 'Y', 1, 0, '0000-00-00 00:00:00', '', '', 'http://wordpress.org/news/feed/'),
-(3, 'http://wordpress.org/support/', 'Support Forums', '', '', '', 'Y', 1, 0, '0000-00-00 00:00:00', '', '', ''),
-(4, 'http://wordpress.org/extend/plugins/', 'Plugins', '', '', '', 'Y', 1, 0, '0000-00-00 00:00:00', '', '', ''),
-(5, 'http://wordpress.org/extend/themes/', 'Themes', '', '', '', 'Y', 1, 0, '0000-00-00 00:00:00', '', '', ''),
-(6, 'http://wordpress.org/support/forum/requests-and-feedback', 'Feedback', '', '', '', 'Y', 1, 0, '0000-00-00 00:00:00', '', '', ''),
-(7, 'http://planet.wordpress.org/', 'WordPress Planet', '', '', '', 'Y', 1, 0, '0000-00-00 00:00:00', '', '', '');
+CREATE TABLE IF NOT EXISTS `pmt_attempts` (
+  `attempt_id` int(11) NOT NULL AUTO_INCREMENT,
+  `attempt_percent_mark` float NOT NULL,
+  `attempt_time_start` varchar(255) NOT NULL,
+  `attempt_time_end` varchar(255) NOT NULL,
+  `attempt_complete` enum(''Y'',''N'') NOT NULL,
+  `course_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `order_id` int(11) NOT NULL,
+  PRIMARY KEY (`attempt_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1186 ;

d34f4696bd3c4402cbb7e86da58070b9e7b42f05
ilya <ilya4041@yandex.ru>
2016-04-11 02:10:03 +0300

    s

---
+++ t.sql
@@ -0,0 +1,15 @@
+--
+-- Table structure for table `pmt_attempts`
+--
+
+CREATE TABLE IF NOT EXISTS `pmt_attempts` (
+  `attempt_id` int(11) NOT NULL AUTO_INCREMENT,
+  `attempt_percent_mark` float NOT NULL,
+  `attempt_time_start` varchar(255) NOT NULL,
+  `attempt_time_end` varchar(255) NOT NULL,
+  `attempt_complete` enum(''Y'',''N'') NOT NULL,
+  `course_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `order_id` int(11) NOT NULL,
+  PRIMARY KEY (`attempt_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1186 ;

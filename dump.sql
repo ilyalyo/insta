ebf12a4fd5b47e233038a67246a445f263a304f1
ilya <ilya4041@yandex.ru>
2016-04-11 13:26:45 +0300

    s

--- dump.sql
+++ dump.sql
@@ -1,4 +1,4 @@
 #!/bin/sh -xe
 mysql -e "CREATE DATABASE hello_database;"
 mysql -e "Use hello_database;"
-mysql -e "hello_database" < t.sql
+mysql -e "CREATE TABLE IF NOT EXISTS `pmt_attempts` (`attempt_id` int(11) NOT NULL AUTO_INCREMENT, `attempt_percent_mark` float NOT NULL)"
